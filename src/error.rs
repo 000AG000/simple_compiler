2eb6a9eca0ce1bb3dd27a554f3bf1b8f79c591ae
= <416c657861@posteo.de>
2026-03-31 13:09:20 +0200

    adding error file

---
+++ src/error.rs
@@ -0,0 +1,5 @@
+
+/// number of characters to visualize ahead when showing an error
+pub const LOOK_AHEAD:usize = 20;
+/// number of characters to visualize afterwards when showing an error
+pub const LOOK_AFTER:usize = 20;
