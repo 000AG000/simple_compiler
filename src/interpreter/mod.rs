0daf3d9b6b492cb87679f478c99d409a6b309ff1
= <416c657861@posteo.de>
2026-03-31 23:43:20 +0200

    added exec_with_output to api

--- src/interpreter/mod.rs
+++ src/interpreter/mod.rs
@@ -2,4 +2,4 @@ mod frame;
 mod main_interpreter;
 
 pub use crate::error::{ErrorKind, GlobalError, RuntimeErrorKind};
-pub use main_interpreter::{exec,exec_with_output};
+pub use main_interpreter::{exec, exec_with_output};
